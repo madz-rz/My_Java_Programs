ff3d9147719924601e9bd9952c517d18231ff422
madz-rz <mad.raj.sri@gmail.com>
2024-03-04 21:14:48 +1100

    Fourth Commit

--- Cpu.java
+++ Cpu.java
@@ -16,7 +16,7 @@ public int getMemory() {
 }
 public void calculateMemory()
 {
-	System.out.println("function is called from outside");
+	System.out.println("function is called from outside as well as from inside");
 }
 
 }
